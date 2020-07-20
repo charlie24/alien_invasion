810fc937757a242e2decb7c4eae217fa26cbfdc0
Charlie <tarazona.charlie@gmail.com>
2020-07-20 00:52:08 -0500

    dibujando linea de aliensa

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     def __init__(self):
@@ -14,8 +15,12 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self)
+        
+        self.ship    = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens  = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         while True:
@@ -55,6 +60,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
     def _update_bullets(self):
@@ -66,6 +72,19 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _create_fleet(self):
+        alien       = Alien(self)
+        alien_width = alien.rect.width
+        
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x   = available_space_x // (2 * alien_width)
+
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
